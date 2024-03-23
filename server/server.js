2e8ae72aa926a9f748449530a8b3b6bfeb39c77a
Anthony Santiago <anthony.santiago58@yahoo.com>
2024-03-24 08:49:02 +1300

    asdf

--- server/server.js
+++ server/server.js
@@ -7,12 +7,12 @@ server.use(express.json())
 server.use(cors())
 
 server.get('/api/v1/photo', async (req, res) => {
-  const response = await fs.readFile('public/gundam.jpg')
+  const response = await fs.readFile('./public/gundam.jpg')
   res.setHeader('Content-Type', 'image/*')
   res.send(response)
 })
 server.get('/api/v1/about_me',async(req,res) => {
-  const response = await fs.readFile('data/data.json')
+  const response = await fs.readFile('./data/data.json')
   res.setHeader('Content-Type','application/json')
   res.send(JSON.parse(response))
 })
