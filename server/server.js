877bec43630971cf85ddaa18de5534e4707a129c
Anthony Santiago <anthony.santiago58@yahoo.com>
2024-03-24 08:56:06 +1300

    add __dirname

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const fs = require('node:fs/promises')
+const path = require('path')
 const cors = require('cors')
 const server = express()
 server.use(express.urlencoded({ extended: true }))
@@ -7,12 +8,16 @@ server.use(express.json())
 server.use(cors())
 
 server.get('/api/v1/photo', async (req, res) => {
-  const response = await fs.readFile('./public/gundam.jpg')
+  // const response = await fs.readFile('./public/gundam.jpg')
+  const response = await fs.readFile(path.join(__dirname,'public',gundam.jpg))
+
   res.setHeader('Content-Type', 'image/*')
   res.send(response)
 })
 server.get('/api/v1/about_me',async(req,res) => {
-  const response = await fs.readFile('./data/data.json')
+  // const response = await fs.readFile('./data/data.json')
+  const response = await fs.readFile(__dirname,'data','data.json')
+
   res.setHeader('Content-Type','application/json')
   res.send(JSON.parse(response))
 })
