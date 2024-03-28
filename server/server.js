31378d13ca2b2869c97a45de91381c915e2f2e14
Anthony Santiago <anthony.santiago58@yahoo.com>
2024-03-29 08:36:49 +1300

    added property on json data

--- server/server.js
+++ server/server.js
@@ -10,8 +10,7 @@ server.use('/images', express.static(path.join(__dirname,'public','project_image
 
 server.get('/api/v1/photo', async (req, res) => {
   try{
-    // const response = await fs.readFile('./public/gundam.jpg')
-    const response = await fs.readFile(path.join(__dirname,'public','gundam.jpg'))
+    const response = await fs.readFile(path.join(__dirname,'public','display_photo.jpg'))
 
     res.setHeader('Content-Type', 'image/*')
     res.send(response)
@@ -22,7 +21,7 @@ server.get('/api/v1/photo', async (req, res) => {
 })
 server.get('/api/v1/about_me',async(req,res) => {
   try{
-    // const response = await fs.readFile('./data/data.json')
+    
     const response = await fs.readFile(path.join(__dirname,'data','data.json'))
 
     res.setHeader('Content-Type','application/json')
@@ -31,14 +30,7 @@ server.get('/api/v1/about_me',async(req,res) => {
     console.log("error fetching JSON data",e)
   }
 })
-// server.get('/api/v1/project_video',async(req,res)=>{
-  
-//   const response = await fs.readFile(path.join(__dirname,'data','data.json'))
-//   const video = JSON.parse(response).projects.map((data)=>data.video)
-//   const directoryPath = path.join(__dirname, 'public', 'videos',video);
-//   const listOfVideos = await fs.readFile(directoryPath)
-//   res.send(listOfVideos)
-// })
+
 const PORT = 3000
 server.listen(PORT, () => {
   console.log(`Server is listening on Port ${PORT}`)
