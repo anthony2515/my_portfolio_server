2cb3865ce89d048c694cf5b5e4c4f6903b5ba7e7
Anthony Santiago <anthony.santiago58@yahoo.com>
2024-04-05 15:08:07 +1300

    finished all functionality except for education section

--- server/server.js
+++ server/server.js
@@ -48,7 +48,10 @@ const storage = multer.diskStorage({
   },
 })
 const uploadDisplayPhoto = multer({ storage: storage })
-server.post('/api/v1/file',uploadDisplayPhoto.single('file'),async (req, res) => {
+server.post(
+  '/api/v1/file',
+  uploadDisplayPhoto.single('file'),
+  async (req, res) => {
     console.log(req.file)
   }
 )
@@ -60,64 +63,127 @@ const ProjectImageStorage = multer.diskStorage({
   filename: function (req, file, cb) {
     // Sets the file name. This example uses the original file name, but it can be customized
     cb(null, file.originalname)
-  
   },
 })
 const uploadProjectImage = multer({ storage: ProjectImageStorage })
-server.post('/api/v1/projectImage',uploadProjectImage.single('project_image'),async (req, res) => {
+server.post(
+  '/api/v1/projectImage',
+  uploadProjectImage.single('project_image'),
+  async (req, res) => {
     const newProjectImage = req.file.originalname
-    console.log("file objects",req.file)
-    const {imageToReplace, index} = req.body
+    console.log('file objects', req.file)
+    const { imageToReplace, index } = req.body
     fs.unlink(
-      path.join(__dirname, 'public', 'project_images', imageToReplace),(err,data) =>{
-        if(err){
+      path.join(__dirname, 'public', 'project_images', imageToReplace),
+      (err, data) => {
+        if (err) {
           throw err
-        }else{
+        } else {
           console.log(`${data} is succesfully deleted`)
         }
       }
     )
 
-    const data  = await fs.readFile(path.join(__dirname,'data','data.json'))
+    const data = await fs.readFile(path.join(__dirname, 'data', 'data.json'))
     const jsonData = JSON.parse(data)
     jsonData.projects[index].image = newProjectImage
     const updateJson = JSON.stringify(jsonData)
-    console.log("imageStorage",ProjectImageStorage.getFilename)
-    await fs.writeFile(path.join(__dirname,'data','data.json'),updateJson)
+    console.log('imageStorage', ProjectImageStorage.getFilename)
+    await fs.writeFile(path.join(__dirname, 'data', 'data.json'), updateJson)
     //work on data.json projects[index].image get that
   }
 )
-server.post('/api/v1/data',async(req,res) => {
-  const result = JSON.stringify(req.body,null,2)
-  await fs.writeFile(path.join(__dirname,'data','data.json'),result)
+server.post('/api/v1/data', async (req, res) => {
+  const result = JSON.stringify(req.body, null, 2)
+  await fs.writeFile(path.join(__dirname, 'data', 'data.json'), result)
 })
-server.post('/api/v1/addProject',uploadProjectImage.single('image'),async(req,res)=>{
-  const image = req.file.originalname
-  const {project_name,description,github_url}=req.body
-  const tech_used = req.body.tech_used.split(",")
-  const obj = {
-    image,
-    project_name,
-    description,
-    tech_used,
-    github_url
+server.post(
+  '/api/v1/addProject',
+  uploadProjectImage.single('image'),
+  async (req, res) => {
+    const image = req.file.originalname
+    const { project_name, description, github_url } = req.body
+    const tech_used = req.body.tech_used.split(',')
+    const obj = {
+      image,
+      project_name,
+      description,
+      tech_used,
+      github_url,
+    }
+    const readData = await fs.readFile(
+      path.join(__dirname, 'data', 'data.json')
+    )
+    const parseReadData = JSON.parse(readData)
+    parseReadData.projects.unshift(obj)
+    const result = JSON.stringify(parseReadData, null, 2)
+    await fs.writeFile(path.join(__dirname, 'data', 'data.json'), result)
+    res.send('project added successfully')
   }
-  const readData = await fs.readFile(path.join(__dirname,'data','data.json'))
+)
+server.delete('/api/v1/deleteProject', async (req, res) => {
+  const { index, image } = req.body
+  const readData = await fs.readFile(path.join(__dirname, 'data', 'data.json'))
   const parseReadData = JSON.parse(readData)
-  parseReadData.projects.unshift(obj)
-  const result = JSON.stringify(parseReadData,null,2)
-  await fs.writeFile(path.join(__dirname,'data','data.json'),result)
-  res.send("project added successfully")
+  parseReadData.projects.splice(index, 1)
+  const result = JSON.stringify(parseReadData, null, 2)
+  await fs.writeFile(path.join(__dirname, 'data', 'data.json'), result)
+  await fs.unlink(path.join(__dirname, 'public', 'project_images', image))
+  res.send('Project Deleted')
 })
-server.delete('/api/v1/deleteProject',async(req,res) =>{
-  const {index,image} = req.body
-  const readData = await fs.readFile(path.join(__dirname,'data','data.json'))
+
+server.post('/api/v1/techSkill', async (req, res) => {
+  const readData = await fs.readFile(path.join(__dirname, 'data', 'data.json'))
   const parseReadData = JSON.parse(readData)
-  parseReadData.projects.splice(index,1)
+  parseReadData.skills[0].technical_skills.unshift(req.body)
+  const result = JSON.stringify(parseReadData, null, 2)
+  await fs.writeFile(path.join(__dirname, 'data', 'data.json'), result)
+  res.send('Tech Skill has been added')
+})
+server.delete('/api/v1/techSkill', async (req, res) => {
+  const { index } = req.body
+  const readData = await fs.readFile(path.join(__dirname, 'data', 'data.json'))
+  const parseReadData = JSON.parse(readData)
+  parseReadData.skills[0].technical_skills.splice(index, 1)
+  const result = JSON.stringify(parseReadData, null, 2)
+  await fs.writeFile(path.join(__dirname, 'data', 'data.json'), result)
+})
+server.post('/api/v1/softSkill', async (req, res) => {
+  const { soft_skills } = req.body
+  const readData = await fs.readFile(path.join(__dirname, 'data', 'data.json'))
+  const parseReadData = JSON.parse(readData)
+  const split = soft_skills.split(',')
+  for (let x = 0; x < split.length; x++) {
+    parseReadData.skills[0].soft_skills.unshift(split[x])
+  }
   const result = JSON.stringify(parseReadData,null,2)
   await fs.writeFile(path.join(__dirname,'data','data.json'),result)
-  await fs.unlink(path.join(__dirname,'public','project_images',image))
-  res.send("Project Deleted")
+  res.send("Soft skill added successfully")
+})
+server.delete('/api/v1/softSkill',async(req,res) =>{
+  const { index } = req.body
+  const readData = await fs.readFile(path.join(__dirname, 'data', 'data.json'))
+  const parseReadData = JSON.parse(readData)
+  parseReadData.skills[0].soft_skills.splice(index, 1)
+  const result = JSON.stringify(parseReadData, null, 2)
+  await fs.writeFile(path.join(__dirname, 'data', 'data.json'), result)
+})
+server.post('/api/v1/tools',async(req,res) => {
+  const readData = await fs.readFile(path.join(__dirname,'data','data.json'))
+  const parseReadData = JSON.parse(readData)
+  parseReadData.skills[0].tools.unshift(req.body)
+  const result = JSON.stringify(parseReadData,null,2)
+  await fs.writeFile(path.join(__dirname,'data','data.json'),result)
+  res.send("Tools added successfully")
+})
+server.delete('/api/v1/tools',async(req,res) => {
+  const {index} = req.body
+  const readData = await fs.readFile(path.join(__dirname,'data','data.json'))
+  const parseReadData = JSON.parse(readData)
+  parseReadData.skills[0].tools.splice(index,1)
+  const result = JSON.stringify(parseReadData,null,2)
+  await fs.writeFile(path.join(__dirname,'data','data.json'),result)
+
 })
 const PORT = 3000
 server.listen(PORT, () => {
